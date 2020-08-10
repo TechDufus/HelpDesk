7e4f31ad08344105f5f7023b88916dca9881fa8e
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 15:44:38 -0500

    Add help and made an advanced function.

--- Functions/Public/Test-Administrator.ps1
+++ Functions/Public/Test-Administrator.ps1
@@ -1,15 +1,25 @@
 <#
 .SYNOPSIS
     Test to see if the current user / process is being run as an Administrator.
+.DESCRIPTION
+    Test if the credentials running this function are elevated on the local system.
+.EXAMPLE
+    PS> Test-Administrator
+
+    Description
+    -----------
+    This will return a boolean based on if the user is an admin or not.
 .NOTES
     Author: Matthew J. DeGarmo
-    GitHub: https://github.com/matthewjdegarmo
+    Handle: @matthewjdegarmo
+.LINK
+    https://zero.comaround.com/link/19fc2b232c2a4cc149c508d821a4d624/
 #>
 function Test-Administrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    [CmdletBinding()]
+    param (
+        $user = [Security.Principal.WindowsIdentity]::GetCurrent()
+    )
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
-
-
-
 
