21a52e04fb099e041d4d5ccd6caec4b3e5b50fa9
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 15:40:01 -0500

    Made an advanced function.

--- Functions/Public/Get-ADGroupsManagedByUser.ps1
+++ Functions/Public/Get-ADGroupsManagedByUser.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/env pwsh
+
 <#
 .SYNOPSIS
     Generate a list of group names owned by a specified.
@@ -6,16 +8,19 @@
 .PARAMETER Identity
     This is the Identity used by `Get-ADUser` to look up the Distinguished name used by `Get-ADGroup`.
 .EXAMPLE
-    PS>Get-ADGroupsManagedByUser user1
+    PS>Get-ADGroupsManagedByUser mjdegar
 
     Description
     -----------
-    This will generate all groups (Security or Distribution) Managed by `user1`.
+    This will generate all groups (Security or Distribution) Managed by `mjdegar`.
 .NOTES
     Author: Matthew J. DeGarmo
-    GitHub: https://github.com/matthewjdegarmo
+    Handle: @matthewjdegarmo
+.LINK
+    https://zero.comaround.com/link/19fc2b232c2a4cc149c508d821a4d624/
 #>
 function Get-ADGroupsManagedByUser() {
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory,Position=0)]
         [string] $Username
@@ -26,7 +31,7 @@ function Get-ADGroupsManagedByUser() {
         Where-Object {$_.ManagedBy -eq $User_DN} | `
         Select-Object Name
     } catch {
-        Write-Error "$($_.Exception.Message)"
+        Write-Error "$($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
     }
 }
 
