9d3af2bc35f3f905e518581ccb44e03f49f0c15e
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-09-03 13:35:39 -0500

    Updating to only build with new module version.

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -2,14 +2,20 @@
 $PSGalleryModule = Find-Module HelpDesk -Repository PSGallery -ErrorAction SilentlyContinue
 
 #Get current GitHub Manifest version (From cloned repo in AzurePipelines)
-$CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine("HelpDesk_Build","HelpDesk","HelpDesk.psd1"))
+$PipelineSourceModulePath = [System.IO.Path]::Combine('HelpDesk','Build','azure-build.ps1')
+$PipelineSourceModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine('HelpDesk','HelpDesk.psd1'))
 
 #Ensure this is either an initial publish to PSGallery, or that the GitHub version is higher than the PSGallery version
-if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
+if ($PipelineSourceModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
+  Write-Output 'Building Module:...'
+  & ([System.IO.Path]::Combine("$PSScriptRoot",'HelpDesk','Build','azure-build.ps1'))
+  $CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine("HelpDesk_Build","HelpDesk","HelpDesk.psd1"))
+  
   Write-Output "PSGallery: v$($PSGalleryModule.Version)"
   Write-Output "Github: v$($CurrentModule.Version)"
+
   Write-Output "Attempting to publish new version $($CurrentModule.Version) to the PowerShell Gallery."
   Publish-Module -Path $CurrentModule.ModuleBase -Repository PSGallery -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose
 } else {
-  Write-Output "GitHub Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
+  Write-Output "GitHub Module version v$($PipelineSourceModule.Version) | PSGallery Module version v$($PSGalleryModule.Version). Pipeline module version is not greater than the Published PSGallery module version."
 }
