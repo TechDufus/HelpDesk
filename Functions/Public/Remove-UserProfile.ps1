c61a0886e9189661ddd62f531f41c7058d4a2958
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2020-09-18 22:03:02 -0500

    More thorough filter when specifying -Except profiles to NOT delete.

--- Functions/Public/Remove-UserProfile.ps1
+++ Functions/Public/Remove-UserProfile.ps1
@@ -36,19 +36,19 @@
     Handle: @matthewjdegarmo
 #>
 function Remove-UserProfile() {
-    [CmdletBinding(SupportsShouldProcess,ConfirmImpact='High',DefaultParameterSetName='Named')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High', DefaultParameterSetName = 'Named')]
     Param (
-        [Parameter(Mandatory,ParameterSetName='Named')]
+        [Parameter(Mandatory, ParameterSetName = 'Named')]
         [System.String[]] $UserName,
 
-        [Parameter(ParameterSetName='Named')]
-        [Parameter(ParameterSetName='All')]
+        [Parameter(ParameterSetName = 'Named')]
+        [Parameter(ParameterSetName = 'All')]
         [System.String] $ComputerName,
 
-        [Parameter(Mandatory,ParameterSetName='All')]
+        [Parameter(Mandatory, ParameterSetName = 'All')]
         [Switch] $All,
 
-        [Parameter(ParameterSetName='All')]
+        [Parameter(ParameterSetName = 'All')]
         [System.String[]] $Except
     )
 
@@ -60,19 +60,20 @@ function Remove-UserProfile() {
                 $Params = @{}
                 $Confirm = $true
                 if ($PSBoundParameters.ContainsKey('ComputerName')) {
-                    $Params += @{ ComputerName = $ComputerName.ToUpper()}
-                } else {$ComputerName = $env:COMPUTERNAME}
+                    $Params += @{ ComputerName = $ComputerName.ToUpper() }
+                }
+                else { $ComputerName = $env:COMPUTERNAME }
 
                 $AllProfiles = Get-UserProfile @Params
                 if ($PSBoundParameters.ContainsKey('Except')) {
                     $Except | ForEach-Object {
                         $ExceptUser = $_
                         Write-Output "Removing $ExceptUser from the deletion list. Will not delete user: $ExceptUser"
-                        $AllProfiles = $AllProfiles | Where-Object {$_.LocalPath -notlike "*$ExceptUser"}
+                        $AllProfiles = $AllProfiles | Where-Object { $_.LocalPath -notlike "*$ExceptUser*" }
                     }
                 }
                 if ($null -ne $AllProfiles) {
-                    if ($PSCmdlet.ShouldProcess($AllProfiles, "Delete all profiles from this computer except for any exceptions listed.")){
+                    if ($PSCmdlet.ShouldProcess($AllProfiles, "Delete all profiles from this computer except for any exceptions listed.")) {
                         Write-Verbose "Removing all user profiles from computer: $ComputerName"
                         $Total = $AllProfiles.Count
                         $Position = 0
@@ -80,9 +81,9 @@ function Remove-UserProfile() {
                         $AllProfiles | ForEach-Object {
                             $Percent = [int]$(($Position / $Total) * 100)
                             $Progress = @{
-                                Activity = "Removing profile for user: $(Split-Path $_.LocalPath -Leaf)"
-                                Status = "Removing profile $Position of $Total"
-                                PercentComplete = (($Position / $Total) * 100)
+                                Activity         = "Removing profile for user: $(Split-Path $_.LocalPath -Leaf)"
+                                Status           = "Removing profile $Position of $Total"
+                                PercentComplete  = (($Position / $Total) * 100)
                                 CurrentOperation = "$Percent%"
                             }
                             Write-Progress @Progress
@@ -90,26 +91,31 @@ function Remove-UserProfile() {
                             $Position++
                         }
                     }
-                } else {
+                }
+                else {
                     Write-Error "There are no user profiles to remove with the specified criteria. Username: $User Computername: $ComputerName"
                 }
-            } else {
+            }
+            else {
                 $Params = @{ }
                 if ($PSBoundParameters.ContainsKey('ComputerName')) {
-                    $Params += @{ ComputerName = $ComputerName.ToUpper()}
-                } else {$ComputerName = $env:COMPUTERNAME}
+                    $Params += @{ ComputerName = $ComputerName.ToUpper() }
+                }
+                else { $ComputerName = $env:COMPUTERNAME }
                 $Username | ForEach-Object {
                     $User = $_
-                    $UserProfile = Get-UserProfile @Params | Where-Object {$_.LocalPath -match $User}
+                    $UserProfile = Get-UserProfile @Params | Where-Object { $_.LocalPath -match $User }
                     if ($UserProfile) {
                         Write-Verbose "Removing user profile $User on $ComputerName"
                         $UserProfile | Remove-CimInstance -Whatif:$Whatif -Confirm:$Confirm
-                    } else {
+                    }
+                    else {
                         Write-Error "There are no user profiles to remove with the specified criteria. Username: $User Computername: $ComputerName"
                     }
                 }
             }
-        } Catch {
+        }
+        Catch {
             Write-Error "$($_.Exception.Message)"
         }
     }
